62cf0d1ba10425250074fad5b81c58c2e19c504d
Jin Xu <jin.xu@amplitude.com>
2025-11-27 18:08:37 -0800

    fix: update framework

--- Frameworks/AmplitudeSessionReplay.xcframework/ios-arm64/AmplitudeSessionReplay.framework/PrivateHeaders/AMPObjCUtilities.h
+++ Frameworks/AmplitudeSessionReplay.xcframework/ios-arm64/AmplitudeSessionReplay.framework/PrivateHeaders/AMPObjCUtilities.h
@@ -14,4 +14,7 @@ typedef void (^AMPCatchBlock)(NSException *e);
 
 void AMPObjCTryCatch(AMPTryBlock tryBlock, _Nullable AMPCatchBlock catchBlock);
 
+/// Safely calls value(forKey:) and returns nil if the object throws NSUndefinedKeyException
+id _Nullable AMPSafeValueForKey(NSObject *object, NSString *key);
+
 NS_ASSUME_NONNULL_END
